56d75e1f6021e913422ff8de91b897a52cd3ebb0
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-03-13 22:08:25 +0700

    List sort by created time, latest first.

--- NumberRemember.js
+++ NumberRemember.js
@@ -4,49 +4,49 @@ var UniqueNumbers = new Mongo.Collection('uniquenumbers');
 
 if (Meteor.isClient) {
 
-  Session.setDefault('status', "Welcome, folks! let's add new number ");
+    Session.setDefault('status', "Welcome, folks! let's add new number ");
 
-  Template.addForm.helpers({
-    status: function() {
-      return Session.get('status');
-    },
-    numbers: function(){
-      return UniqueNumbers.find();
-    }
-  });
-
-  Template.addForm.events({
-    
-    'keypress input.newNumber': function(evt, template) {
-      if (evt.which === 13) {
-        var newNumber = template.find(".newNumber").value;
-        console.log('Inputing: ' + newNumber);
-
-        // check with collection
-        var existNumber = UniqueNumbers.findOne({
-          value: newNumber
-        });
-
-        if (!existNumber) {
-          UniqueNumbers.insert({
-            value: newNumber,
-            createdAt: new Date()
-          });
-          template.find(".newNumber").value = "";
-          Session.set('status', 'Inserted!');
-        } else {
-          template.find(".newNumber").value = "";
-          Session.set('status', 'Sorry, this number is exist!');
+    Template.addForm.helpers({
+        status: function() {
+            return Session.get('status');
+        },
+        numbers: function() {
+            return UniqueNumbers.find({}, { sort: { createdAt: -1 } });
         }
-      } else {
-          // live check while typing the number
-      }
-    }
-  });
+    });
+
+    Template.addForm.events({
+
+        'keypress input.newNumber': function(evt, template) {
+            if (evt.which === 13) {
+                var newNumber = template.find(".newNumber").value;
+                console.log('Inputing: ' + newNumber);
+
+                // check with collection
+                var existNumber = UniqueNumbers.findOne({
+                    value: newNumber
+                });
+
+                if (!existNumber) {
+                    UniqueNumbers.insert({
+                        value: newNumber,
+                        createdAt: new Date()
+                    });
+                    template.find(".newNumber").value = "";
+                    Session.set('status', 'Inserted!');
+                } else {
+                    template.find(".newNumber").value = "";
+                    Session.set('status', 'Sorry, this number is exist!');
+                }
+            } else {
+                // live check while typing the number
+            }
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function() {
-    // code to run on server at startup
-  });
+    Meteor.startup(function() {
+        // code to run on server at startup
+    });
 }
